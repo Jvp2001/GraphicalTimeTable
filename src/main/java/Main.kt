a4b6c1de8f46c4ba6c498a54b1f47d856da2dfac
jvp2001 <thisisminecraftpetersen@gmail.com>
2019-11-15 16:39:12 +0000

    Initial Commit.

---
+++ src/main/java/Main.kt
@@ -0,0 +1,20 @@
+import javafx.application.Application
+import javafx.stage.Stage
+
+class Main : Application()
+{
+    override fun start(primaryStage: Stage?)
+    {
+
+    }
+
+    companion object
+    {
+        @JvmStatic
+        fun main(args: Array<String>)
+        {
+            launch(*args)
+        }
+    }
+
+}
